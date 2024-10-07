eb2a92ad33f6c89807ad0a9824b5de6a94c21387
Tron <109302537+MrDonaldGlov@users.noreply.github.com>
2024-10-07 16:10:55 +0000

    library instalation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,15 @@
 import "../styles/styles.css";
-// Add imports here
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 console.log("What?");
 console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
-  // Put code that runs ONLY in the browser here -- this is most likely where
+Aos.init();
   // MOST of your code should go.
   //
   // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
 }
+
+import "@picocss/pico/css/pico.min.css";
